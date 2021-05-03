012069686f2c577c1d775b290ae8e427d403c88b
Taks7 <72123380+Taks7@users.noreply.github.com>
2021-05-03 17:33:08 +0200

    UI

--- Mercs/Source/ModulePlayer.h
+++ Mercs/Source/ModulePlayer.h
@@ -52,6 +52,7 @@ public:
 	// The player spritesheet loaded into an SDL_Texture
 	SDL_Texture* texture = nullptr;
 	SDL_Texture* merc = nullptr;
+	SDL_Texture* mercLife = nullptr;
 	// The pointer to the current player animation
 	// It will be switched depending on the player's movement direction
 	Animation* currentAnimation = nullptr;
