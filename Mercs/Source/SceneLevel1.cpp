fa16d8e1adc6b93696fa7f02fe869ff1d6ac1399
Alejandromo125 <72094186+Alejandromo125@users.noreply.github.com>
2021-06-04 10:05:41 +0200

    Update SceneLevel1.cpp

--- Mercs/Source/SceneLevel1.cpp
+++ Mercs/Source/SceneLevel1.cpp
@@ -233,8 +233,7 @@ bool SceneLevel1::Start()
 	//BOSS
 	App->enemies->AddEnemy(Enemy_Type::BOSS, 756 - 120, 50 - 1100);
 
-	App->enemies->AddEnemy(Enemy_Type::ENEMY_CAR, 220 - 120, 1340 - 1100);
-	App->enemies->AddEnemy(Enemy_Type::TOWER_ENEMY, 200 - 120, 1340 - 1100);
+	
 	
 	//App->render->camera.x = 0;
 	//App->render->camera.y = 0;
