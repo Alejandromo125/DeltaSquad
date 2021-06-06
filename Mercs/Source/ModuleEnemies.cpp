386d703a4673c354e2624f771c3065c2373af34f
Alejandromo125 <alejandromartinortega@gmail.com>
2021-06-06 11:36:02 +0200

    Update ModuleEnemies.cpp

--- Mercs/Source/ModuleEnemies.cpp
+++ Mercs/Source/ModuleEnemies.cpp
@@ -144,6 +144,20 @@ void ModuleEnemies::HandleEnemiesSpawn()
 				SpawnEnemy(spawnQueue[i]);
 				spawnQueue[i].type = Enemy_Type::NO_TYPE; // Removing the newly spawned enemy from the queue
 			}
+			if (spawnQueue[i].x * SCREEN_SIZE > App->render->camera.x - (App->render->camera.w * SCREEN_SIZE) - SPAWN_MARGIN)
+			{
+				LOG("Spawning enemy at %d", spawnQueue[i].x * SCREEN_SIZE);
+
+				SpawnEnemy(spawnQueue[i]);
+				spawnQueue[i].type = Enemy_Type::NO_TYPE; // Removing the newly spawned enemy from the queue
+			}
+			if (spawnQueue[i].y * SCREEN_SIZE > App->render->camera.y - (App->render->camera.h * SCREEN_SIZE) - SPAWN_MARGIN)
+			{
+				LOG("Spawning enemy at %d", spawnQueue[i].y * SCREEN_SIZE);
+
+				SpawnEnemy(spawnQueue[i]);
+				spawnQueue[i].type = Enemy_Type::NO_TYPE; // Removing the newly spawned enemy from the queue
+			}
 		}
 	}
 }
